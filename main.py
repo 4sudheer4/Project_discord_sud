dd2419664e6dd9340a18360521b9cf1e1e2fff16
4sudheer4 <sudheeranugoju4@gmail.com>
2022-11-19 23:44:41 -0500

    using bot and client functionalities in same code

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+import dislash
 import discord
 import os
+from discord.ext import commands
+import json
+
+
+if os.path.exists(os.getcwd()+"/config.json"):
+    with open('./config.json') as f:
+        configData = json.load(f)
+else:
+    configTemplate = {"Token":"","prefix":"!"}
+
+    with open(os.getcwd()+"/config.json","w+") as f:
+        json.dump(configTemplate,f)
+
+token = configData["Token"]
 
 client = discord.Client(intents=discord.Intents.default())
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+intents_ = discord.Intents.default()
+intents_.members = True
 
-@client.event
+bot = commands.Bot(command_prefix ="!", intents = intents_)
+
+
+
+
+@bot.event 
+async def on_ready():
+    print("bot is ready")
+
+@bot.command()
+async def ping(ctx):
+    latency = bot.latency
+    await ctx.send(f"pong! {latency}")
+
+
+
+# @bot.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+
+@bot.event
 async def on_message(message):
+    # to avoid sync issues between on_message and bot.command() (below line)
+    await bot.process_commands(message)
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-# token refresh needed?
 
-client.run('MTAzOTY0NzE2NjY1NTg4OTUyOA.GsmTwo.4k8evbtwGbxIPXEyEo5v9DqGtn9Hvd9qJ4SwoU')
+# commands.Bot is a subclass of discord.Client, meaning commands.Bot inherits all of the functionality of discord.Client
+
+# You can't really combine them because they're the same thing. commands.Bot is the extended version of discord.Client.
+
+bot.run(token)
+# client.run(token)
